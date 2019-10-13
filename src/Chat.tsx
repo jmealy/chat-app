4c3060e1c35ec8b0b982bf208c102bd8fb3c96b9
James <jamesmealy1@gmail.com>
2019-10-13 12:11:14 +0200

    feat: UI components for message window

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -3,10 +3,37 @@ import './Chat.css';
 
 type ChatProps = {};
 
+type Message = {
+  author: string,
+  text: string,
+  timeStamp: string
+};
+
 const Chat = ({}: ChatProps) => {
 
+  const messages = [
+    { author: "Anonymous", text: "hi there this is a message for the chat", timeStamp: '17.07'},
+    { author: "Anonymous", text: "hi there this is another  message for the chat, how you like that?", timeStamp: '17.07'},
+    { author: "Anonymous", text: "hi there", timeStamp: '17.07'}
+  ]
+
+  const renderMessage = (message: Message) => (
+    <div className="message">
+      <div className="messageHeader">
+        <p className="author">{message.author}</p>
+        <p className="timeStamp">{message.timeStamp}</p>
+      </div>
+      <p className="messageBody">{message.text}</p>
+    </div>
+  );
+
   return (
-    <div >
+    <div className="chatContainer">
+      {
+        messages.map((message, index) => (
+          renderMessage(message)
+        ))
+      }
       
     </div>
   );
